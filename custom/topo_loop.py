a613186a18b91934aa362be25e5cecfd7eedfae3
Srini Seetharaman <srini.seetharaman@gmail.com>
2016-03-30 02:10:11 -0700

    hostPing and netPing

---
+++ custom/topo_loop.py
@@ -0,0 +1,21 @@
+from mininet.topo import Topo
+
+class LoopTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+        s1=self.addSwitch("s1")
+        s2=self.addSwitch("s2")
+        s3=self.addSwitch("s3")
+
+        h1=self.addHost("h1")
+        h2=self.addHost("h2")
+        h3=self.addHost("h3")
+
+        self.addLink(s1,h1)
+        self.addLink(s2,h2)
+        self.addLink(s3,h3)
+        self.addLink(s1,s2)
+        self.addLink(s2,s3)
+        self.addLink(s3,s1)
+
+topos = { 'loop_topo': ( lambda: LoopTopo() ) }
